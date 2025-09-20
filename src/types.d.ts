c12496a7d37672d0678717e8b3dc1537116df6d0
Ducko <duckodas@gmail.com>
2025-09-20 14:59:45 +0200

    Update: types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,17 +1,34 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, ClientEvents } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, ClientEvents, RESTEvents, Message } from 'discord.js';
 
-export interface EventInterface<K extends keyof ClientEvents = keyof ClientEvents> {
-  name: K;
-  options?: {
-    rest?: boolean;
-    once?: boolean;
-  };
-  execute: (...args: ClientEvents[K]) => Promise<void> | void;
+export type ObjectNameIDArray = Array[{ name: string; id: string }];
+
+export interface EventInterface<K extends keyof ClientEvents | keyof RESTEvents = keyof ClientEvents> {
+	name: K;
+	options?: { once?: boolean; rest?: boolean };
+	execute: (
+		...args: K extends 'interactionCreate'
+			? [client: any, interaction: ChatInputCommandInteraction]
+			: K extends keyof ClientEvents
+				? [client: any, ...ClientEvents[K]]
+				: K extends keyof RESTEvents
+					? [client: any, ...RESTEvents[K]]
+					: [client: any, ...any[]]
+	) => void | Promise<void>;
 }
 
-export interface CommandInterface {
-  cooldown: number;
-  isDeveloperOnly: boolean;
-  data: SlashCommandBuilder;
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void> | void;
+export interface SlashCommandInterface {
+	cooldown: number;
+	isDeveloperOnly: boolean;
+	data: SlashCommandBuilder;
+	execute: (client: any, interaction: ChatInputCommandInteraction) => Promise<void> | void;
+}
+
+export interface PrefixCommandInterface {
+	name: string;
+	description?: string;
+	aliases?: string[];
+	usage?: string;
+	cooldown?: number;
+	isDeveloperOnly?: boolean;
+	execute: (client: any, message: Message, args: string[]) => void | Promise<void | Message>;
 }
